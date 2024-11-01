3f6eba5068a14a7c04ef2495dfebd209f0f97180
Harold Kim <me@harold.kim>
2024-11-02 02:25:13 +0900

    Add global stores, add favicon, fix layout

---
+++ static/maps.js
@@ -0,0 +1,59 @@
+
+const getLastMod = (dateString) => {
+    const pad = num => String(num).padStart(2, '0');
+    const date = new Date(Date.parse(dateString));
+    const year = date.getFullYear();
+    const month = pad(date.getMonth() + 1);
+    const day = pad(date.getDate());
+    const hour = pad(date.getHours());
+    const min = pad(date.getMinutes());
+    const sec = pad(date.getSeconds());
+    return `${year}/${month}/${day} ${hour}:${min}:${sec}`;
+}
+
+const map = L.map('map', {preferCanvas: true}).setView([36, 138], 6);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+const markers = L.markerClusterGroup();
+const locationIcon = new L.Icon({
+    iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+    shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+});
+
+const chooseGame = async (gameName) => {
+    markers.clearLayers();
+    const response = await fetch(`./json/${gameName}.json`);
+    
+    const lastModified = document.querySelector("#last-modified");
+    lastModified.innerHTML = `Database: ${getLastMod(response.headers.get("Last-Modified"))}`;
+    
+    const stores = await response.json();
+    stores.forEach(store => {
+        const marker = L.marker(store.location, { icon: locationIcon });
+        marker.bindPopup(`
+            <div class="notranslate store-info">
+                <font size="4"><b>${store.name}</b></font>
+                <br><br>
+                <font size="3">${store.address}</font>
+                <br><br>
+                <ul>
+                    <li>
+                        <font size="3"><a target="_new" href="https://maps.google.com/maps?q=${store.name}@${store.location.join(",")}&zoom=16&hl=en">Google Maps</a></font>
+                    </li>
+                    <li>
+                        <font size="3"><a target="_new" href="https://map.yahoo.co.jp/search?q=${store.name}&lat=${store.location[0]}&lng=${store.location[1]}&zoom=16&hl=en">Yahoo Maps (JP)</a></font>
+                    </li>
+                </ul>
+            </div>
+        `).openPopup();
+        markers.addLayer(marker);
+    });
+    map.addLayer(markers);
+};
+
+document.addEventListener('DOMContentLoaded', () => chooseGame("duplicate"));
--- storemap.py
+++ storemap.py
@@ -12,74 +12,119 @@ import re
 import json
 import requests
 
+
 STORES = {
-    "ongeki": "https://location.am-all.net/alm/location?gm=88&at={prefecture}&ct=1000",
-    "chunithm": "https://location.am-all.net/alm/location?gm=109&lang=en&ct=1000&at={prefecture}",
-    "maimai": "https://location.am-all.net/alm/location?gm=96&lang=en&ct=1000&at={prefecture}",
+    "ongeki": {
+        "JP": "https://location.am-all.net/alm/location?gm=88&at={prefecture}&ct=1000",
+    },
+    "chunithm": {
+        "JP": "https://location.am-all.net/alm/location?gm=109&lang=en&ct=1000&at={prefecture}",
+        "EN": "https://location.am-all.net/alm/location?gm=104&lang=en&ct={country}",
+    },
+    "maimai": {
+        "JP": "https://location.am-all.net/alm/location?gm=96&lang=en&ct=1000&at={prefecture}",
+        "EN": "https://location.am-all.net/alm/location?gm=98&lang=en&ct={country}",
+    },
 }
+
+
+def find_duplicate_store(store_a, store_b):
+    """ (list of dict, list of dict) -> list of dict
+
+    Find duplicate dictionaries in two lists of dictionaries
+    """
+    result = []
+    _tmp = set((_find['name'], tuple(_find['address'])) for _find in store_a)
+    for _find in store_b:
+        if (_find['name'], tuple(_find['address'])) in _tmp:
+            result.append(_find)
+    return result
+
 
 def dupe_stores():
     """
     Find duplicate stores
     """
-    def _find_dupe_store(store_a, store_b):
-        """ (list of dict, list of dict) -> list of dict
-
-        Find duplicate dictionaries in two lists of dictionaries
-        """
-        result = []
-        _tmp = set((_find['name'], tuple(_find['address'])) for _find in store_a)
-        for _find in store_b:
-            if (_find['name'], tuple(_find['address'])) in _tmp:
-                result.append(_find)
-        return result
+    # filter by country
+    def filter_country(store_list, country):
+        return list(filter(lambda d: d['country'] == country.upper(), store_list))
 
     # Load store files
-    _stores = []
-    for store in STORES:
-        with open(f"json/{store}.json", encoding="utf-8") as store_file:
-            _stores.append(json.loads(store_file.read()))
-    # Use _find_dupe_store to catch duplicates
-    result = _stores[0]
-    for i in range(1, len(_stores)):
-        result = _find_dupe_store(result, _stores[i])
+    stores = {}
+    for game_type in STORES:
+        with open(f"json/{game_type}.json", encoding="utf-8") as store_file:
+            stores[game_type] = json.loads(store_file.read())
+
+    # find duplicate store to catch duplicates
+    result = []
+    for country in ["JP", "EN"]:
+        available_games = [i for i in STORES if STORES[i].get(country)]
+
+        tmp = filter_country(stores[available_games[0]], country)
+        for game_type in available_games:
+            tmp = find_duplicate_store(tmp, filter_country(stores[game_type], country))
+        result.extend(tmp)
+
     # Save to file
     with open("json/duplicate.json", mode="w", encoding="utf-8") as store_file:
         store_file.write(json.dumps(result))
     return True
 
+
+def crawl_location(store_urls):
+    """ (str) -> json
+
+    Crawl from location.am-all.net
+    """
+    result = []
+
+    # Grab all prefectures (JP)
+    if store_urls.get("JP"):
+        for prefecture in range(47):
+            # Parse URLs, get addresses
+            response = requests.get(store_urls["JP"].format(prefecture=prefecture)).text
+            store_location = re.findall(r'//maps.google.com/maps\?q=(.*)\&zoom', response)
+            store_address = re.findall(r'<span class="store_address">(.*)</span>', response)
+            for i, _raw in enumerate(store_location):
+                store_name, store_location = _raw.split("@")
+                store_location = [float(i) for i in store_location.split(",")]
+                result.append({
+                    'name': store_name,
+                    'address': store_address[i],
+                    'location': store_location,
+                    'country': 'JP'
+                })
+
+    # Grab all countries (EN)
+    if store_urls.get("EN"):
+        for country in range(1000, 1020):
+            response = requests.get(store_urls["EN"].format(country=country)).text
+            store_location = re.findall(r'//maps.google.com/maps\?q=(.*)\&zoom', response)
+            store_address = re.findall(r'<span class="store_address">(.*)</span>', response)
+            for i, _raw in enumerate(store_location):
+                store_name, store_location = _raw.rsplit("@", 1)
+                store_location = [float(i) for i in store_location.split(",")]
+                result.append({
+                    'name': store_name,
+                    'address': store_address[i],
+                    'location': store_location,
+                    'country': 'EN'
+                })
+    return result
+
+
 def crawl_stores():
     """
     Crawl stores and save to file
     """
-    def _crawl_location(url):
-        """ (str) -> json
 
-        Crawl from location.am-all.net
-        """
-        result = []
-        # Grab all prefectures
-        for _prefecture in range(47):
-            # Parse URLs, get addresses
-            resp_text = requests.get(url.format(prefecture=_prefecture)).text
-            _loc = re.findall(r'//maps.google.com/maps\?q=(.*)\&zoom', resp_text)
-            _addr = re.findall(r'<span class="store_address">(.*)</span>', resp_text)
-            for i, _raw in enumerate(_loc):
-                _name, _location = _raw.split("@")
-                _location = [float(i) for i in _location.split(",")]
-                result.append({
-                    'name': _name,
-                    'address': _addr[i],
-                    'location': _location,
-                })
-        return result
-
-    for store_name, store_url in STORES.items():
-        with open(f"json/{store_name}.json", mode="w", encoding="utf-8") as store_file:
-            _result = _crawl_location(store_url)
-            store_file.write(json.dumps(_result))
+    for game_name, store_urls in STORES.items():
+        with open(f"json/{game_name}.json", mode="w", encoding="utf-8") as store_file:
+            result = crawl_location(store_urls)
+            store_file.write(json.dumps(result))
 
     return True
+
 
 if __name__ == "__main__":
     crawl_stores()
